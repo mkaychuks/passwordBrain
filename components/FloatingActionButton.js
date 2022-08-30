12dd0fbed6aa9f0170cb316cca54a78561ae471a
mkaychuks <sachufusi@gmail.com>
2022-08-30 13:25:53 +0100

    FAB

--- components/FloatingActionButton.js
+++ components/FloatingActionButton.js
@@ -2,7 +2,7 @@ import { Pressable, StyleSheet } from "react-native";
 import React from "react";
 import { EvilIcons } from "@expo/vector-icons";
 
-const FloatingActionButton = ({onPressHandler}) => {
+const FloatingActionButton = ({ onPressHandler }) => {
   return (
     <Pressable style={styles.fab} onPress={onPressHandler}>
       <EvilIcons name="pencil" size={40} color="#fff" />
