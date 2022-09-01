1ef8f30b8e90ac5a128ca19e4eba80f043e6c031
mkaychuks <sachufusi@gmail.com>
2022-09-01 14:11:13 +0100

    formatted the api call

--- services/getHeadlines.js
+++ services/getHeadlines.js
@@ -1,15 +1,32 @@
 import axios from "axios";
 
-const BASE_URL = "https://saurav.tech/NewsAPI/top-headlines/category/sports/us.json"
+const BASE_URL =
+  "https://saurav.tech/NewsAPI/top-headlines/category/sports/us.json";
 
+const formatHeadlineData = (data) => {
+  let formattedData = [];
+
+  data.forEach((item, index) => {
+    const format = {
+      ...item,
+      id: index.toString(),
+    };
+
+    formattedData.push(format);
+  });
+
+  return formattedData;
+};
 
 const getHeadlinesData = async () => {
-    try{
-        const response = await axios.get(BASE_URL)
-        const data = response.data
-        return data.articles.slice(0, 25)
-    } catch(err){
-        console.log(err)
-    }
-}
-export default getHeadlinesData
+  try {
+    const response = await axios.get(BASE_URL);
+    const data = response.data;
+    const fData = data.articles.slice(0, 25);
+    return formatHeadlineData(fData);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default getHeadlinesData;
