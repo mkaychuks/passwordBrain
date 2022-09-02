c14325a9568f1630aacd939acd7fdd4cd97bd760
mkaychuks <sachufusi@gmail.com>
2022-09-02 10:02:11 +0100

    added firestore

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,6 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app) // initialize authentication for the app
+const db = getFirestore(app) // initialize the storage db
 
 
-export { auth, app}
+export { auth, app, db}
